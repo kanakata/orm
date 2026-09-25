0e65de6fa5495831b9f1bc38e7e2edb84c2960d7
kanakata <patrick37668@gmail.com>
2026-09-26 02:10:44 +0300

    2026-09-25 23:10:44

--- test.php
+++ test.php
@@ -1,10 +1,2 @@
 <?php
 include "./query_builder.php";
-// print_r((new QueryBuilder())->table("products")->select()->order_by(["id"], ["desc"])->find_all());
-// print_r((new QueryBuilder())->table("products")->select()->order_by(["id"], ["desc"])->limit(10,2)->find_all());
-// print_r((new QueryBuilder())->table("products")->select()->limit(10,2)->find_all());
-// print_r((new QueryBuilder())->table("products")->count()->count_column());
-// print_r((new QueryBuilder())->table("products")->avg("id")->count_column());
-// print_r((new QueryBuilder())->table("products")->sum("id")->count_column());
-// print_r((new QueryBuilder())->table("products")->min("id")->count_column());
-// print_r((new QueryBuilder())->table("products")->max("id")->count_column());
